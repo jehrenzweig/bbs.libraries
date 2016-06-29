5c3a34b62d5115e290313a2df36cd4aa7b3f09ab
Brenton Reeder <brenton.reeder@betabytesoftware.com>
2016-06-28 21:35:05 -0400

    Created a test / demo app

---
+++ BBS.Libraries.ConsoleAppTests.Emails/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BBS.Libraries.Emails;
+
+namespace BBS.Libraries.ConsoleAppTests.Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generate and send an email
+
+            var model = new Models.Razor01()
+            {
+                ToEmailAddressCollection = new BBS.Libraries.Emails.EmailAddressCollection("brenton@helpfulcowz.com"),
+                FromEmailAddress = new EmailAddress("someone@somewhere.net"),
+                FirstName = "Brenton"
+            };
+
+            var razorEngine = new BBS.Razor.RazorContentEmailGenerator<Models.Razor01>(
+                BBS.Libraries.ConsoleAppTests.Emails.T4Files.RepositryNamespace,
+                BBS.Libraries.ConsoleAppTests.Emails.T4Files.Views.Razor._01_Subject_cshtml,
+                BBS.Libraries.ConsoleAppTests.Emails.T4Files.Views.Razor._01_PlainText_cshtml,
+                BBS.Libraries.ConsoleAppTests.Emails.T4Files.Views.Razor._01_Mhtml_cshtml
+                );
+
+            razorEngine.GenerateAndSend(model);
+        }
+    }
+}
---
+++ BBS.Libraries.ConsoleAppTests.Emails/Readme.txt
@@ -0,0 +1,7 @@
+﻿Just wanted to drop a quick note about this email templating here.
+
+To make this work, you will need to embed the views into the application (it's already done for you in this demo console app).
+
+Also, please note that the T4Template's namespaces need to be updated - check the T4LocalContent.settings.t4 file for the settings.
+
+Have fun!
